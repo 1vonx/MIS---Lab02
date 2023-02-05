cfd12d32830cf6aa6b875abd57006bb6cbc21737
Ivona Minkovska <94076658+1vonx@users.noreply.github.com>
2023-02-05 21:15:56 +0100

    Lab2

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import './models/question.dart';
+import './widget/clothing_answer.dart';
+import './widget/clothing_question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  void _iWasTapped() {
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
+
+  var questions = [
+    Question(
+      questionText: "Select clothing store",
+      answers: [
+        'Zara',
+        'Bershka',
+        'Stradivarius',
+      ],
+    ),
+    Question(
+      questionText: "Select category",
+      answers: [
+        'Dresses',
+        'Jeans',
+        'Shirts',
+        'Shoes',
+      ],
+    ),
+    Question(
+      questionText: "Select color range",
+      answers: [
+        'black - white',
+        'yellow - red',
+        'green - purple',
+        'other'
+      ],
+    ),
+  ];
+  var _questionIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MIS - Lab02",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Clothing App"),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            PizzaQuestion(questions[_questionIndex].questionText),
+            ...(questions[_questionIndex].answers).map((answer) {
+              return PizzaAnswer(_iWasTapped, answer);
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
